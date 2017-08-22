eb7143aa64d116eab4e297b14c1352527c915b60
lingdian <lingdian@meili-inc.com>
2017-08-22 22:52:39 +0800

    Initial Commit

---
+++ ProjectObjectC09/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProjectObjectC09
+//
+//  Created by lingdian on 17/8/22.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
